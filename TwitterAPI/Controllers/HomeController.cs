b2cd38ab9ba9c147be4b9990de9642ee5cf057df
Fernando <lfgc1@softtek.com>
2020-01-30 15:22:57 -0600

    Lastversion

--- TwitterAPI/Controllers/HomeController.cs
+++ TwitterAPI/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace TwitterAPI.Controllers
         public ActionResult Index()
         {
             //instanciar modelo y llenarlo con informacion
-            //remplazar con API de Twitter y traer hash
+            //remplazar con API de Twitter y traer hash   
             Auth.SetUserCredentials("OmnRr6g9E4nIdy5vW6IE9gJmD", "iY9kah88jg5Hw53qsfQ5UGzpif0V0mOK30Ar5ie2pBfVpFyHan"
                                    , "796462679758163971-NubG7GJva0P1BttmpQoGkot71YTc6f9"
                                    , "aODZ3wD1xD6lxifeWZPO4TzjY7ybD5gYojdMiQXDhEZjt");
