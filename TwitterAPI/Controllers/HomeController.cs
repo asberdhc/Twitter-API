4e08657999d0a2080a96adb64b0c24d581fb1375
Fernando <lfgc1@softtek.com>
2020-01-30 10:41:21 -0600

    Se agrega vista parcial, listado de twittts

--- TwitterAPI/Controllers/HomeController.cs
+++ TwitterAPI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TwitterAPI.Models;
 
 namespace TwitterAPI.Controllers
 {
@@ -10,6 +11,29 @@ namespace TwitterAPI.Controllers
     {
         public ActionResult Index()
         {
+            //instanciar modelo y llenarlo con informacion
+            //remplazar con API de Twitter y traer hash
+            var tweets = new List<Hash_tagModel>()
+            {
+                new Hash_tagModel()
+                {
+                    Hash = "#Enero",
+                    FechaPublicacion = Convert.ToDateTime("2020/01/29"),
+                    Usuario = "fer"
+                },
+                new Hash_tagModel()
+                {
+                Hash = "#Febrero",
+                FechaPublicacion = Convert.ToDateTime("2020/01/01"),
+                Usuario = "Luisfer"
+            }
+            };
+
+            //enviar la lista por medio de ViewBag para implementar en parcialView
+
+            ViewBag.ListaHashs = tweets; //se le asigna la lista con datos
+
+
             return View();
         }
 
@@ -17,6 +41,22 @@ namespace TwitterAPI.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+            var tweets = new List<Hash_tagModel>()
+            {
+                new Hash_tagModel()
+                {
+                    Hash = "#Mexico",
+                    FechaPublicacion = Convert.ToDateTime("2020/01/29"),
+                    Usuario = "user1"
+                },
+                new Hash_tagModel()
+                {
+                    Hash = "#USA",
+                    FechaPublicacion = Convert.ToDateTime("2020/01/01"),
+                    Usuario = "user2"
+                }
+            };
+            ViewBag.ListaHashs = tweets; //se le asigna la lista con datos
             return View();
         }
 
---
+++ TwitterAPI/Models/Hash_tagModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterAPI.Models
+{
+    public class Hash_tagModel
+    {
+        public string Hash { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string Usuario { get; set; }
+    }
+}
